512a9fc41e2df6e771ae47f33d92beb2f8ed94de
abo <linjianfengqrh@gmail.com>
2014-07-14 22:34:36 -0700

    Add wrapper on load and dump

---
+++ include/json.hpp
@@ -0,0 +1,21 @@
+#ifndef __JCONER_JSON_HPP__
+#define __JCONER_JSON_HPP__
+
+#include "value.hpp"
+#include "stream.hpp"
+#include "parser.hpp"
+#include "token.hpp"
+
+#include <iostream>
+
+namespace JCONER {
+
+void dump(JValue* value, std::ostream& out);
+void dump(JValue* value, std::ostream& out, PrintProperty& property);
+
+JValue* load(std::string filename);
+JValue* load(const char* filename);
+JValue* loads(const char* buffer);
+
+}
+#endif
---
+++ src/json.cpp
@@ -0,0 +1,16 @@
+#include "json.hpp"
+
+namespace JCONER {
+
+JValue* load(std::string filename) {
+    FileIStream fin(filename);
+    Parser parser(fin);
+    return parser.parse();
+}
+
+JValue* load(const char* filename) {
+    std::string str_filename(filename);
+    return load(str_fileanme);
+}
+
+}
